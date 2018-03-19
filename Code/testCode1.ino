7a7ad134d65fb955ecd1fcc51a81eca0f5743656
Mattijs Coens <Mattijs.coens@student.howest.be>
2018-03-19 14:48:46 +0100

    Added testCode1&2

---
+++ Code/testCode1.ino
@@ -0,0 +1,96 @@
+/*
+ # Library:AutoWatering
+   Library Functions:
+   1.Power a pump to water flowers according to the humidity and temperature
+   2.Use the DHT11 Sensor to test the ambient humidity and temperature
+   3.Use the Moisture Sensor to test the moisture of the soil
+
+ # Hardwares:
+ 1. Auto flower watering control board
+ 2. DHT11 Teaperature and Humidity Sensor
+ 3. Moisture Sensor
+ 4. Immersible pump
+
+ # Connection:
+ 1.DHT11 & Free Life v1.0 board
+    S      ---   Digital Pin9
+   VCC     ---   VCC
+   GND     ---   GND
+
+ 2.Moisture Sensor(Humidity) & Free Life v1.0 board
+    1S      ---   Analog Pin2
+   2GND     ---   GND
+   3VCC     ---   VCC
+*/
+
+
+#include <AutoWatering.h>
+#include <DHT.h>
+
+#define MaxTemprature 100  //The Maximum Value of the Temperature
+#define SensorTest 1
+#define CarbonTest 0
+
+AutoWatering flower;
+
+void setup()
+{
+  flower.Initialization();//Initialization for the watering kit
+  Serial.begin(115200);//Buad Rate is set as 115200bps
+}
+void loop()
+{
+  //Power on the pump according to the ambient temperature and humidity
+  pumpOn(SensorTest,CarbonTest,MaxTemprature);
+  //Power on the pump for testing whether the pump can work properly
+  //flower.pumpTestOn();
+  delay(2000);
+  flower.pumpOff();
+  delay(2000);
+  Serial.println();
+}
+
+//Power on the pump according to the ambient temperature and humidity
+void pumpOn(int MoiSensor, int Carbon, int Temperature_max)
+{
+   int humidity;
+   int humidity_max;
+   float dht_t;
+
+   //Choose to use the Moisture Sensor or the Carbon
+   //to test the soil moisture
+   if(MoiSensor==1&&Carbon==0)
+   {
+    humidity = flower.MoistureSensor();
+   }else if(MoiSensor==0&&Carbon==1)
+   {
+    humidity = flower.CarbonRod();
+   }else{
+    humidity = 0;
+   }
+   Serial.print("Soil Moisture is :");
+   Serial.println(humidity);
+
+   humidity_max = flower.ADJ_humMax();
+
+   dht_t = flower.getTemperature();
+   Serial.print("Temperature is :");
+   Serial.println(dht_t);
+
+   digitalWrite(6,LOW);
+   digitalWrite(5,LOW);
+   digitalWrite(7,LOW);
+   digitalWrite(4,LOW);
+
+   if(humidity<=humidity_max&&dht_t<=Temperature_max)
+    {
+      digitalWrite(6,HIGH);
+      digitalWrite(5,HIGH);
+     }
+   else
+    {
+      digitalWrite(6,LOW);
+      digitalWrite(5,LOW);
+    }
+    Serial.println("Pump is on!");
+}
---
+++ Code/testCode2.ino
@@ -0,0 +1,67 @@
+/*
+ # This Sample code is for testing the Auto flower watering kit.
+ # Product: EcoDuino - An auto plant kit
+ # SKU    : KIT0003
+
+ # Library:AutoWatering
+   Library Functions:
+   1.Power a pump to water flowers according to the humidity and temperature
+   2.Use the DHT11 Sensor to test the ambient humidity and temperature
+   3.Use the Moisture Sensor to test the moisture of the soil
+
+ # Hardwares:
+ 1. Auto flower watering control board
+ 2. DHT11 Teaperature and Humidity Sensor
+ 3. Moisture Sensor
+ 4. Immersible pump
+
+ # Connection:
+ 1.DHT11 & Free Life v1.0 board
+    S      ---   Digital Pin9
+   VCC     ---   VCC
+   GND     ---   GND
+
+ 2.Moisture Sensor(Humidity) & Free Life v1.0 board
+    1S      ---   Analog Pin2
+   2GND     ---   GND
+   3VCC     ---   VCC
+*/
+
+#include <AutoWatering.h>
+#include <DHT.h>
+#define MaxTemprature 40  //The Maximum Value of the Temperature
+#define Sensor 1
+#define Carbon 0
+
+AutoWatering flower;
+
+int temperature;
+int moisture_dat;
+int humidity;
+
+void setup()
+{
+  flower.Initialization();//Initialization for the watering kit
+  Serial.begin(115200);//Buad Rate is set as 115200bps
+}
+void loop()
+{
+  //Obtain the Temperasture from DHT11 Sensor
+  temperature = flower.getTemperature();
+  Serial.print("Temperature is:" );
+  Serial.println(temperature);
+
+ //Obtain the Humidity from DHT11 Sensor
+  humidity = flower.getHumidity();
+  Serial.print("Humidity is:" );
+  Serial.println(humidity);
+
+ //Obtain the Soil Moisture from the Moisture Sensor
+  moisture_dat = flower.MoistureSensor();
+  Serial.print("Soil Moisture is:" );
+  Serial.println(moisture_dat);
+
+  Serial.println();
+  Serial.println();
+  delay(1500);
+}
